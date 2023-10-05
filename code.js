f1307087d84666b954849659618eed86bf730717
Sean W <104464288+swilso59@users.noreply.github.com>
2023-10-04 18:14:51 -0600

    Update code.js
    I am unsure if this is the way we are suppose to be doing the reverse insertion sort. But the logic seems to make sense to me since we are starting at the end of the array and working our way to the zero index and ordering the values of the array in descending order.

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
-function insertionSortReverse(array) {
-    return array;
+function insertionSortReverse(arr) {
+  for (var i = arr.length - 2; i >= 0; i--) {
+    var val = arr[i];
+    var j;
+    for (j = i; j < arr.length - 1 && arr[j + 1] > val; j++) {
+      arr[j] = arr[j + 1];
+    }
+    arr[j] = val;
+  }
+  return arr;
 }
+
+// Example usage:
+var arr = [5, 2, 9, 3, 6];
+insertionSortReverse(arr);
+console.log(arr); // Output: [9, 6, 5, 3, 2] 
